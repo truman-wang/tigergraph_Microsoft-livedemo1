2f577d775ceb7ea6aebe2745fe6a8e14d503e06a
Yuchen Zhang <yuchen.zhang@tigergraph.com>
2022-07-20 19:33:15 +0000

    improve

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
-c() {
 for q in *.gsql ; do  
   gsql $q
 done
@@ -9,7 +8,6 @@ gsql -g ldbc_snb install query all
 for q in is6 is6r is6d is6dr ; do
 ./demo.py $q commentId.csv
 done
-}
 
 for q in is2 is2r is2d is2dr ; do
 ./demo.py $q personId.csv
