a6fcb185a39069dc282560eccd7b43d8212c73c7
Yuchen Zhang <yuchen.zhang@tigergraph.com>
2022-07-21 01:02:51 +0000

    small change

--- run.sh
+++ run.sh
@@ -5,10 +5,11 @@ done
 
 gsql -g ldbc_snb install query all
 
-for q in is6 is6r is6d ; do
-./demo.py $q commentId.csv
-done
 
 for q in is2 is2r is2d ; do
 ./demo.py $q personId.csv
+done
+
+for q in is6 is6r is6d ; do
+./demo.py $q commentId.csv
 done
